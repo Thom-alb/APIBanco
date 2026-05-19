c9a1ea7c7a60714628e76e220131bdf6a60004c0
Thom-Alb <thomasaulinskialbeche@gmail.com>
2026-05-19 20:59:43 -0300

    Praticando usar git com um trabalho do curso :)

---
+++ BancoAPIS/src/main/java/BancoAPIS/BancoApisApplication.java
@@ -0,0 +1,13 @@
+package BancoAPIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoApisApplication.class, args);
+	}
+
+}
---
+++ BancoAPIS/src/test/java/BancoAPIS/BancoApisApplicationTests.java
@@ -0,0 +1,13 @@
+package BancoAPIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
